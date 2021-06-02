8eed8ea58f76d7050fcdd2301088c23b9fdb15c7
walidkhire1 <57401889+walidkhire1@users.noreply.github.com>
2021-06-02 16:25:24 +0100

    Test

--- Solution1/WebApplication1/Startup.cs
+++ Solution1/WebApplication1/Startup.cs
@@ -16,9 +16,9 @@ namespace WebApplication1
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-          var x= "OKJKLOJKKLJPKJKJLKJ";
+            var x = "OKJKLOJKKLJPKJKJLKJ";
             var y = x;
-            Console.Error.Write(x);
+            Console.Error.Write(x);.
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
