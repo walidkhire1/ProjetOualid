10969b28402235f5ce7f298989f3bd288faf12f0
walidkhire1 <57401889+walidkhire1@users.noreply.github.com>
2021-06-02 16:39:27 +0100

    .

--- Solution1/WebApplication1/Program.cs
+++ Solution1/WebApplication1/Program.cs
@@ -14,6 +14,7 @@ namespace WebApplication1
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
